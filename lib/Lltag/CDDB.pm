92656863131204de51ba0c1051bf58c7050f165f
bgoglin <bgoglin@b20c0237-0900-0410-9b41-d09804560211>
2006-09-10 06:47:14 +0000

    rework CDDB menu by splitting into one function for each level - fix buggy track index selection

--- lib/Lltag/CDDB.pm
+++ lib/Lltag/CDDB.pm
@@ -12,11 +12,22 @@ use vars qw(@EXPORT) ;
 	      get_cddb_tags
 	      ) ;
 
+# return values that are passed to lltag
 my $CDDB_SUCCESS = 0 ;
 my $CDDB_ABORT = -1 ;
 
+# local return values
+my $CDDB_ABORT_TO_KEYWORDS = -10 ;
+my $CDDB_ABORT_TO_CDIDS = -11 ;
+
 my $server_host = "www.freedb.org" ;
 my $server_port = 80 ;
+
+my $previous_cdids = undef ;
+my $previous_cd = undef ;
+
+#########################################3
+# low level CDDB http requests
 
 sub cddb_query_cd_by_keywords {
     my $keywords = shift ;
@@ -103,6 +114,10 @@ sub cddb_query_tracks_by_id {
 	}
     }
 
+    close $socket ;
+
+    return undef unless defined $name ;
+
     # TODO: are we sure no artist or album may contain " / " ?
     $name =~ m@^(.+) / (.+)$@ ;
     $cd->{ARTIST} = $1 ;
@@ -110,28 +125,11 @@ sub cddb_query_tracks_by_id {
 
     # TODO: check number and indexes of tracks ?
 
-    close $socket ;
-
     return $cd ;
 }
 
-my $previous_cdids = undef ;
-my $previous_cd = undef ;
-
-sub cddb_keywords_usage {
-  print "    <category>/<hexadecinal id> => CDDB query for CD matching category and id\n" ;
-  print "    <space-seperated keywords> => CDDB query for CD matching the keywords\n" ;
-  print "    q => Quit CDDB query\n" ;
-  print "    h => Show this help\n" ;
-}
-
-sub cddb_cd_usage {
-  print "    <index> => Choose a CD in the current keywords query results list\n" ;
-  print "    v => View the list of CD matching the keywords\n" ;
-  print "    k => Start again CDDB query with different keywords\n" ;
-  print "    q => Quit CDDB query\n" ;
-  print "    h => Show this help\n" ;
-}
+######################################################
+# interactive menu to browse CDDB, tracks in a CD
 
 sub cddb_track_usage {
   print "    <index> => Choose a track of the current CD\n" ;
@@ -142,83 +140,8 @@ sub cddb_track_usage {
   print "    h => Show this help\n" ;
 }
 
-sub get_cddb_tags {
-    my $reply ;
-    my $cdids ;
-    my $cdid ;
-    my $cd ;
-
-    # FIXME: check additional parameter to start from scratch
-    if (defined $previous_cd) {
-	$cdids = $previous_cdids ;
-	$cd = $previous_cd ;
-	goto CD_RESULTS ;
-    }
-    if (defined $previous_cdids) {
-	$cdids = $previous_cdids ;
-	goto KEYWORDS_RESULTS ;
-    }
-
-    # enter keywords for a query
-  KEYWORDS:
-    my $keywords = Lltag::Misc::readline ("  ", "Enter CDDB query (<query>,qh)", "", -1) ;
-    chomp $keywords ;
-    goto KEYWORDS if $keywords eq '' ;
-    if ($keywords eq 'h') {
-	cddb_keywords_usage () ;
-	goto KEYWORDS ;
-    }
-    return ($CDDB_ABORT, undef) if $keywords eq 'q' ;
-
-    # it this actually a categorie/id ?
-    if ($keywords =~ m@^\s*(\w+)/([\da-f]+)\s*$@) {
-	$cdid->{CAT} = $1 ;
-	$cdid->{ID} = $2 ;
-	goto CD_QUERY ;
-    }
-
-    # do the actual query for CD id with keywords
-    $keywords =~ s/ /+/g ;
-    $cdids = cddb_query_cd_by_keywords $keywords ;
-    $previous_cdids = $cdids ;
-    $previous_cd = undef ;
-    goto KEYWORDS unless @{$cdids} ;
-
-  KEYWORDS_RESULTS:
-    # print the resulting CDs
-    my $cdid_format = "    %0".(length (scalar @{$cdids}))."d: %s (cat=%s, id=%s)\n" ;
-    for(my $i=0; $i < @{$cdids}; $i++) {
-	my $cdid = $cdids->[$i] ;
-	printf ($cdid_format, $i+1, $cdid->{NAME}, $cdid->{CAT}, $cdid->{ID}) ;
-    }
-
-    # choose a CD id
-  CD:
-    Lltag::Misc::print_question "  Enter CD index (<index>,vkqh) ? " ;
-    $reply = <> ;
-    chomp $reply ;
-    goto CD if $reply eq '' ;
-    goto KEYWORDS if $reply eq 'k' ;
-    goto KEYWORDS_RESULTS if $reply eq 'v' ;
-    return ($CDDB_ABORT, undef) if $reply eq 'q' ;
-    if ($reply !~ /^\d+$/ or $reply < 1 or $reply > @{$cdids}) {
-	cddb_cd_usage () ;
-	goto CD ;
-    }
-
-    # do the actual query for CD contents
-    $cdid = $cdids->[$reply-1] ;
-  CD_QUERY:
-    $cd = cddb_query_tracks_by_id ($cdid->{CAT}, $cdid->{ID}, $cdid->{NAME}) ;
-    $previous_cd = $cd ;
-
-    if (!$cd->{TRACKS}) {
-	print "    There is no tracks in this CD.\n" ;
-	goto CD ;
-    }
-
-  CD_RESULTS:
-    # print the CD contents
+sub print_cd {
+    my $cd = shift ;
     map {
 	print "    $_: $cd->{$_}\n" ;
     } grep { $_ !~ /^\d+$/ } (keys %{$cd}) ;
@@ -227,33 +150,164 @@ sub get_cddb_tags {
 	my $track = $cd->{$i+1} ;
 	printf ($track_format, $i+1, $track->{TITLE}, $track->{TIME}) ;
     }
+}
 
-    # choose a track
-  TRACK:
-    Lltag::Misc::print_question "  Enter track index (<index>,vckqh) ? " ;
-    $reply = <> ;
-    chomp $reply ;
-    goto TRACK if $reply eq '' ;
-    goto KEYWORDS if $reply eq 'k' ;
-    goto KEYWORDS_RESULTS if $reply eq 'c' ;
-    goto CD_RESULTS if $reply eq 'v' ;
-    return ($CDDB_ABORT, undef) if $reply eq 'q' ;
-    if ($reply !~ /^\d+$/ or $reply < 1 or $reply > $cd->{TRACKS}) {
+sub get_cddb_tags_from_tracks {
+    my $cd = shift ;
+
+    print_cd $cd ;
+
+    while (1) {
+	Lltag::Misc::print_question "  Enter track index (<index>,vckqh) ? " ;
+	my $reply = <> ;
+	chomp $reply ;
+	next if $reply eq '' ;
+
+	return ($CDDB_ABORT, undef) if $reply eq 'q' ;
+	return ($CDDB_ABORT_TO_KEYWORDS, undef) if $reply eq 'k' ;
+	return ($CDDB_ABORT_TO_CDIDS, undef) if $reply eq 'c' ;
+
+	if ($reply eq 'v') {
+	    print_cd $cd ;
+	    next ;
+	} ;
+
+	if ($reply =~ /^\d+$/ and $reply >= 1 and $reply <= $cd->{TRACKS}) {
+	    # get the track tags
+	    my $track = $cd->{$reply} ;
+	    my %values ;
+	    $values{ARTIST} = $cd->{ARTIST} ;
+	    $values{TITLE} = $track->{TITLE} ;
+	    $values{ALBUM} = $cd->{ALBUM} ;
+	    $values{NUMBER} = $reply ;
+	    $values{GENRE} = $cd->{GENRE} if defined $cd->{GENRE} ;
+	    $values{DATE} = $cd->{YEAR} if defined $cd->{YEAR} ;
+	    return ($CDDB_SUCCESS, \%values) ;
+	}
+
 	cddb_track_usage () ;
-	goto TRACK ;
+    }
+}
+
+##########################################################
+# interactive menu to browse CDDB, CDs in a query results
+
+sub cddb_cd_usage {
+  print "    <index> => Choose a CD in the current keywords query results list\n" ;
+  print "    v => View the list of CD matching the keywords\n" ;
+  print "    k => Start again CDDB query with different keywords\n" ;
+  print "    q => Quit CDDB query\n" ;
+  print "    h => Show this help\n" ;
+}
+
+sub print_cdids {
+    my $cdids = shift ;
+
+    my $cdid_format = "    %0".(length (scalar @{$cdids}))."d: %s (cat=%s, id=%s)\n" ;
+    for(my $i=0; $i < @{$cdids}; $i++) {
+	my $cdid = $cdids->[$i] ;
+	printf ($cdid_format, $i+1, $cdid->{NAME}, $cdid->{CAT}, $cdid->{ID}) ;
+    }
+}
+
+sub get_cddb_tags_from_cdid {
+    my $cdid = shift ;
+    my $cd = cddb_query_tracks_by_id ($cdid->{CAT}, $cdid->{ID}, $cdid->{NAME}) ;
+
+    if (!$cd or !$cd->{TRACKS}) {
+	print "    There is no tracks in this CD.\n" ;
+	goto AGAIN ;
     }
 
-    # print the track tags
-    my $track = $cd->{$reply-1} ;
-    my %values ;
-    $values{ARTIST} = $cd->{ARTIST} ;
-    $values{TITLE} = $track->{TITLE} ;
-    $values{ALBUM} = $cd->{ALBUM} ;
-    $values{NUMBER} = $reply ;
-    $values{GENRE} = $cd->{GENRE} if defined $cd->{GENRE} ;
-    $values{DATE} = $cd->{YEAR} if defined $cd->{YEAR} ;
+    $previous_cd = $cd ;
 
-    return ($CDDB_SUCCESS, \%values) ;
+    return get_cddb_tags_from_tracks $cd ;
+}
+
+sub get_cddb_tags_from_cdids {
+    my $cdids = shift ;
+
+  AGAIN:
+    print_cdids $cdids ;
+    while (1) {
+	Lltag::Misc::print_question "  Enter CD index (<index>,vkqh) ? " ;
+	my $reply = <> ;
+	chomp $reply ;
+	next if $reply eq '' ;
+
+	return ($CDDB_ABORT, undef) if $reply eq 'q' ;
+	return ($CDDB_ABORT_TO_KEYWORDS, undef) if $reply eq 'k' ;
+	goto AGAIN if $reply eq 'v' ;
+
+	if ($reply =~ /^\d+$/ and $reply >= 1 and $reply <= @{$cdids}) {
+	    # do the actual query for CD contents
+	    my ($res, $values) = get_cddb_tags_from_cdid $cdids->[$reply-1] ;
+	    goto AGAIN if $res == $CDDB_ABORT_TO_CDIDS ;
+	    return ($res, $values) ;
+	}
+
+	cddb_cd_usage () ;
+    }
+}
+
+##########################################################
+# interactive menu to browse CDDB, keywords query
+
+sub cddb_keywords_usage {
+  print "    <category>/<hexadecinal id> => CDDB query for CD matching category and id\n" ;
+  print "    <space-seperated keywords> => CDDB query for CD matching the keywords\n" ;
+  print "    q => Quit CDDB query\n" ;
+  print "    h => Show this help\n" ;
+}
+
+sub get_cddb_tags {
+
+    # FIXME: check an additional parameter to start from scratch
+
+    if (defined $previous_cd) {
+	bless $previous_cd ;
+	my ($res, $values) = get_cddb_tags_from_tracks $previous_cd ;
+	return ($res, $values) if $res == $CDDB_SUCCESS or $res == $CDDB_ABORT ;
+	if ($res == $CDDB_ABORT_TO_CDIDS) {
+	    bless $previous_cdids ;
+	    my ($res, $values) = get_cddb_tags_from_cdids $previous_cdids ;
+	    return ($res, $values) if $res == $CDDB_SUCCESS or $res == $CDDB_ABORT ;
+	}
+    }
+
+    while (1) {
+	my $keywords = Lltag::Misc::readline ("  ", "Enter CDDB query (<query>,qh)", "", -1) ;
+	chomp $keywords ;
+	next if $keywords eq '' ;
+
+	return ($CDDB_ABORT, undef) if $keywords eq 'q' ;
+
+	if ($keywords eq 'h') {
+	    cddb_keywords_usage () ;
+	    next ;
+	}
+
+	# it this a category/id ?
+	if ($keywords =~ m@^\s*(\w+)/([\da-f]+)\s*$@) {
+	    my $cdid ;
+	    $cdid->{CAT} = $1 ;
+	    $cdid->{ID} = $2 ;
+	    # FIXME: do not show 'c' for goto to CD list in there
+	    my ($res, $values) = get_cddb_tags_from_cdid $cdid ;
+	    return ($res, $values) if $res == $CDDB_SUCCESS or $res == $CDDB_ABORT ;
+	    next ;
+	}
+
+	# do the actual query for CD id with keywords
+	$keywords =~ s/ /+/g ;
+	my $cdids = cddb_query_cd_by_keywords $keywords ;
+	$previous_cdids = $cdids ;
+	$previous_cd = undef ;
+
+	my ($res, $values) = get_cddb_tags_from_cdids $cdids ;
+	next if $res == $CDDB_ABORT_TO_KEYWORDS ;
+	return ($res, $values) ;
+    }
 }
 
 1 ;
